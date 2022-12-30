fe992392afba1a54a58c7608eff3143cd14ba985
kkkyloo <92991764+kkkyloo@users.noreply.github.com>
2022-12-30 04:57:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(window).on('load', function () {
-    $(".loader").fadeOut();
-    $("#preloder").delay(0).fadeOut("slow");
-});
+// $(window).on('load', function () {
+//     $(".loader").fadeOut();
+//     $("#preloder").delay(0).fadeOut("slow");
+// });
