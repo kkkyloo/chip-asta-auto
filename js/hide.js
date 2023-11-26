784ef4a4f0f208d47c775dc9d9e1dc6b58299a0b
kkkyloo <92991764+kkkyloo@users.noreply.github.com>
2023-11-26 18:20:54 +0300

    Add files via upload

--- js/hide.js
+++ js/hide.js
@@ -2,16 +2,26 @@ function toggleText() {
     var moreText = document.getElementById("moreText");
     var btnText = document.getElementById("toggleBtn");
     var imgElement = document.querySelector('.img_none');
+    var mobNavbar = window.getComputedStyle(navmob);
 
     var computedStyle = window.getComputedStyle(moreText);
 
-    if (computedStyle.display === "none") {
+    if (computedStyle.display === "none" && mobNavbar.display === "none" ) {
         moreText.style.display = "inline";
         btnText.innerHTML = "Скрыть";
-        imgElement.style.height = '60vh';
-    } else {
+        imgElement.style.height = '70vh';
+    } 
+    else if (computedStyle.display === "inline" && mobNavbar.display === "none" ) {
         moreText.style.display = "none";
         btnText.innerHTML = "Подробнее";
         imgElement.style.height = '36vh';
+    }
+    else if (computedStyle.display === "none" && mobNavbar.display === "block" ) {
+        moreText.style.display = "inline";
+        btnText.innerHTML = "Скрыть";
+    }
+    else{
+        moreText.style.display = "none";
+        btnText.innerHTML = "Подробнее";
     }
 }
